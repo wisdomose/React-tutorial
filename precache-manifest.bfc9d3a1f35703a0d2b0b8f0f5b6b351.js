645063963eec2577838e9e4d886fb7383b230ae9
wisdomose <35114555+wisdomose@users.noreply.github.com>
2020-05-06 04:18:09 +0100

    Add files via upload

---
+++ precache-manifest.bfc9d3a1f35703a0d2b0b8f0f5b6b351.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ab4e33389776e531a4fdffc4e5e8f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce34832b2496d776605e",
+    "url": "/static/css/2.75ec312b.chunk.css"
+  },
+  {
+    "revision": "650da0465aa9dba7dc5c",
+    "url": "/static/css/main.a3870e68.chunk.css"
+  },
+  {
+    "revision": "ce34832b2496d776605e",
+    "url": "/static/js/2.d7130bc4.chunk.js"
+  },
+  {
+    "revision": "9765d9b4636b1746cc23259113d1ae44",
+    "url": "/static/js/2.d7130bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650da0465aa9dba7dc5c",
+    "url": "/static/js/main.0a017266.chunk.js"
+  },
+  {
+    "revision": "031b8524f7c2ea82c588",
+    "url": "/static/js/runtime-main.76b4324f.js"
+  },
+  {
+    "revision": "7085058d349a5101d4404a9ccca7f15a",
+    "url": "/static/media/avatar.7085058d.jpg"
+  },
+  {
+    "revision": "f96e3f103ffa871bdaebe7094a93245f",
+    "url": "/static/media/boy.f96e3f10.jpg"
+  },
+  {
+    "revision": "cec1deba0d1616a1db82920c484eb21a",
+    "url": "/static/media/success.cec1deba.jpg"
+  }
+]);
