ad55e7b33ce8b6b71dd7c378914153da799f085e
wisdomose <35114555+wisdomose@users.noreply.github.com>
2020-05-09 18:52:26 +0100

    Add files via upload

---
+++ precache-manifest.bcfc6d5c40eb9132eff126c8aeef5994.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3f2c79e988470e74e02ef1be87b8d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95510c5909c234cdd833",
+    "url": "/static/css/2.75ec312b.chunk.css"
+  },
+  {
+    "revision": "a290364f970bcfa10493",
+    "url": "/static/css/main.c7d99027.chunk.css"
+  },
+  {
+    "revision": "95510c5909c234cdd833",
+    "url": "/static/js/2.b5508cf6.chunk.js"
+  },
+  {
+    "revision": "f05a9da55f8862af6e537620307b99cd",
+    "url": "/static/js/2.b5508cf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a290364f970bcfa10493",
+    "url": "/static/js/main.7ded568f.chunk.js"
+  },
+  {
+    "revision": "031b8524f7c2ea82c588",
+    "url": "/static/js/runtime-main.76b4324f.js"
+  },
+  {
+    "revision": "7eab013f218076a15f0e6e63254bdf2b",
+    "url": "/static/media/avatar.7eab013f.jpg"
+  },
+  {
+    "revision": "29c9e1ef51131693ad952e45f6a8cba6",
+    "url": "/static/media/boy.29c9e1ef.jpg"
+  },
+  {
+    "revision": "35ecffea79f442ce10bc44563f36381e",
+    "url": "/static/media/facebook.35ecffea.svg"
+  },
+  {
+    "revision": "1af7781a66f262728a8b305b1584dd12",
+    "url": "/static/media/home.1af7781a.svg"
+  },
+  {
+    "revision": "ca11cb4c46387177d052585308d69db5",
+    "url": "/static/media/image.ca11cb4c.svg"
+  },
+  {
+    "revision": "02184a3d03718bfa2cce4af188b6b55f",
+    "url": "/static/media/instagram.02184a3d.svg"
+  },
+  {
+    "revision": "3b2662aa6b0a0b1fd6adcc716e887a52",
+    "url": "/static/media/laptop.3b2662aa.svg"
+  },
+  {
+    "revision": "e854fe635f95408768e189e3370d2972",
+    "url": "/static/media/phone.e854fe63.svg"
+  },
+  {
+    "revision": "b75a6782340765a158a567de9703add2",
+    "url": "/static/media/question.b75a6782.svg"
+  },
+  {
+    "revision": "379581a1f26d024a73c753c41844db8d",
+    "url": "/static/media/sign-in.379581a1.svg"
+  },
+  {
+    "revision": "cec1deba0d1616a1db82920c484eb21a",
+    "url": "/static/media/success.cec1deba.jpg"
+  },
+  {
+    "revision": "8121b6eb89c81811d52f8894a6d71bc3",
+    "url": "/static/media/twitter.8121b6eb.svg"
+  },
+  {
+    "revision": "985ffb64aae4b2a4040ccf22ad93be74",
+    "url": "/static/media/user.985ffb64.svg"
+  },
+  {
+    "revision": "03ae97c899c933cb1eed8e501032a740",
+    "url": "/static/media/whatsapp.03ae97c8.svg"
+  }
+]);
